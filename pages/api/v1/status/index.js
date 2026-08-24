ec17f99f6a6de1fdf8a46a1f0ac05401fa47d8a8
Matheus <matheuscorrea9@gmail.com>
2026-08-24 17:34:48 -0300

    add `database version` to status page`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,19 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1;");
-  console.log(result.rows);
-  response.status(200).json({ key: "value" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersion.rows[0].server_version;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+      },
+    },
+  });
 }
 
 export default status;
